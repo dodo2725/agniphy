1e88b5928593e36951c99b87181ce929f08e2eba
dodo2725 <agnitrabhattacharjee01@gmail.com>
2025-10-24 13:10:46 +0530

    Update boards.js

--- website/boards.js
+++ website/boards.js
@@ -1,10 +1,5 @@
-// Board selection logic
-function selectBoard(boardName) {
-  // Save selected board in localStorage for later
-  localStorage.setItem('selectedBoard', boardName);
-  // Redirect to syllabus page
-  window.location.href = 'syllabus.html';
+function selectBoard(board) {
+  localStorage.setItem("selectedBoard", board);
+  window.location.href = "class.html";
 }
 
-// Expose function globally
-window.selectBoard = selectBoard;
